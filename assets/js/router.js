7938fa7f63addf622bf1e0e33d03963182059ded
PRAVINR0 <pk3419391@gmail.com>
2025-11-23 18:14:01 +0530

    c

--- assets/js/router.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const router = (()=>{
-  function getQuery(){return Object.fromEntries(new URLSearchParams(location.search))}
-  function navigateTo(url){location.href=url}
-  return {getQuery,navigateTo};
-})();
