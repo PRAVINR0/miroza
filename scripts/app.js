ee99ca73ca2b4ef818287a36821b77c8b25462fa
PRAVINR0 <pk3419391@gmail.com>
2025-12-01 07:40:22 +0530

    c

--- scripts/app.js
+++ scripts/app.js
@@ -929,4 +929,13 @@
   };
   window.MIROZA.buildCard = window.MIROZA.builder.build;
 
+  /* Service Worker Registration */
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('SW registered:', reg.scope))
+        .catch(err => console.log('SW registration failed:', err));
+    });
+  }
+
 })();
